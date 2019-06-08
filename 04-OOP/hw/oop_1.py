ab2fa15328a3fa14a38e5959fa660af737c8e413
Komarovskikh Daniil <34663647+Wdywfm@users.noreply.github.com>
2019-06-08 13:03:58 +0300

    add solution for oop_1

--- 04-OOP/hw/oop_1.py
+++ 04-OOP/hw/oop_1.py
@@ -40,6 +40,40 @@ PEP8 соблюдать строго, проверку делаю автотес
 import datetime
 
 
+class Homework:
+    def __init__(self, text, deadline, created):
+        self.text = text
+        self.deadline = deadline
+        self.created = created
+
+    def is_active(self):
+        return (datetime.datetime.now() - self.created) < self.deadline
+
+
+class Student:
+    def __init__(self, last_name, first_name):
+        self.last_name = last_name
+        self.first_name = first_name
+
+    @staticmethod
+    def do_homework(homework):
+        if homework.is_active():
+            return homework
+        else:
+            return 'You are late'
+
+
+class Teacher:
+    def __init__(self, last_name, first_name):
+        self.last_name = last_name
+        self.first_name = first_name
+
+    @staticmethod
+    def create_homework(text, days):
+        deadline = datetime.timedelta(days)
+        return Homework(text, deadline, datetime.datetime.now())
+
+
 if __name__ == '__main__':
     teacher = Teacher('Daniil', 'Shadrin')
     student = Student('Roman', 'Petrov')
